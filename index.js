4281db7e7b69d319fc46fa420ec8847cb7242ada
Devin Elder <xcidis.de@gmail.com>
2018-10-12 11:24:19 -0700

    revision: table to array, simplified.

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const prompts = require('prompts');
 const X = `x   x\n x x\n  x\n x x\nx   x`;
 const O = ` ooo \no   o\no   o\no   o\n ooo `;
 var game = ['','','','','','','','',''];
-var table = `\n_________________________\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n_________________________\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n_________________________\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n|       |       |       |\n_________________________`;
-var current = 'x';
+var table = [`\n_________________________\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n_________________________\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n_________________________\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n`,`|       |       |       |\n_________________________`];
+var Game = {
+    table,
+    game,
+    current: ''
+}
 
 async function Choose(){
     var prompt = await prompts({
@@ -14,9 +18,9 @@ async function Choose(){
         message: 'Player 1, choose your letter. (X or O)',
         validate: letter => letter === 'x' || letter === "X" || letter === 'o' || letter === 'O' ? true : `Sorry that's not an option.`
     })
-    
-    console.log(`Player 2.....`); 
+    console.log('\n\nPlace your letter on the board with keys 1-9 left to right top to bottom.', Game.table.join(''));
     setTimeout(()=>{
+        console.log(`Player 2.....`); 
         console.log(`you are ${prompt.letter == 'x' || prompt.letter == 'X' ? 'O' : 'X'}`)
     },750);
 }
